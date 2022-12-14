2c0a4346673c0976ba8cee67b0e4b05fa1d1cba8
Eunseo <90231153+Eunseo-jo@users.noreply.github.com>
2022-12-14 05:12:23 -0600

    Add files via upload
    more completed version than previous

--- index.js
+++ index.js
@@ -7,9 +7,7 @@
 */
 
 const APIs = (() => {
-
     const URL = "http://localhost:3000/todos";
-
     const addTodo = (newTodo) => { //create task
         return fetch(URL, {
             method: "POST",
@@ -19,14 +17,11 @@ const APIs = (() => {
     };
 
     const editTodo = (id, newTodo) => { //update task
-        
         return fetch(URL + `/${id}`, {
             method: "PATCH",
             body: JSON.stringify(newTodo),
             headers: { "Content-Type": "application/json" },
-
         }).then((res) => res.json());
-        
     };
 
     const removeTodo = (id) => { //delete task
@@ -49,13 +44,11 @@ const APIs = (() => {
 const Model = (() => {
     //todolist
     class State {
-        #todos; //[{id: ,title: },{}]
+        #todos; //[{id: ,title: , status: },{}]
         #onChange;
-        #setStatus;
 
         constructor() {
             this.#todos = [];
-            this.#setStatus = "pending";
         }
 
         get todos() {
@@ -63,21 +56,11 @@ const Model = (() => {
         }
 
         set todos(newTodo) {
-            console.log("setter");
+            console.log("this.#onChange?");
             this.#todos = newTodo;
             this.#onChange?.();
         }
 
-        get todostatus() //return status
-        {
-            return this.#setStatus;
-        }
-
-        set todostatus(newStatus) {
-            console.log("set status");
-            this.#setStatus = newStatus;
-            this.#setStatus?.();
-        }
         subscribe(callback) {
             this.#onChange = callback;
             this.setStatus = callback;
@@ -98,54 +81,73 @@ const Model = (() => {
 const View = (() => {
     const formEl = document.querySelector(".form"); //querying
     const todoListEl = document.querySelector(".todo-list");
-
+    const finishedEl = document.querySelector(".finished"); //finished To-do list
     const updateTodoList = (todos) => {
 
         let template = "";
-        todos.forEach((todo) => {
-            const todoTemplate = `<li> <input class = "todo-input" id= ${todo.id}> <span>${todo.title}</span><button class="btn--editFin" id="${todo.id}">edit</button><button class="btn--edit" id="${todo.id}">edit</button><button class="btn--delete" id="${todo.id}">remove</button></li>`;
-            template += todoTemplate;
+        let finishedTemplate = "";
+        todos.sort((a,b) => b.id - a.id).forEach((todo) => {
+            if (todo.status == true) //finished = true
+            {
+                const todofinTemplate = `<li id= "${todo.id}" class = "checked"> <input class = "todo-input" id= "${todo.id}"> <span id="${todo.id}">${todo.title}</span> <div class="buttons"> <button class="btn--editFin" id="${todo.id}">edit</button><button class="btn--edit" id="${todo.id}">edit</button><button class="btn--delete" id="${todo.id}">remove</button> </div> </li>`;
+                finishedTemplate += todofinTemplate;
+            }
+            else {
+                const todoTemplate = `<li id= "${todo.id}" > <input class = "todo-input" id= "${todo.id}"> <span id="${todo.id}">${todo.title}</span><div class="buttons"><button class="btn--editFin" id="${todo.id}">edit</button><button class="btn--edit" id="${todo.id}">edit</button><button class="btn--delete" id="${todo.id}">remove</button></div></li>`;
+                template += todoTemplate;
+            }
         });
-        if(todos.length === 0){
+        if(template.length === 0){
             template = "no task to display"
         }
         todoListEl.innerHTML = template;
+        finishedEl.innerHTML = finishedTemplate;
         
     };
-
-    const statusTodoList = (todos) => {
-        todoListEl.addEventListener('click', function(event) {
-            if (event.target.tagName === 'LI') {
-                event.target.classList.toggle('checked');
-            }
-          }, false);
-    };
-
+    
     const editmode = (todos) => {
         todoListEl.addEventListener('click', function(event) {
             const id = event.target.id;
-            
-            if(event.target.className === "btn--edit"){
-
-                document.querySelector(".todo-list span").style.display = 'none';
-                document.querySelector('.todo-input').style.display = 'block';
-                document.querySelector('.btn--editFin').style.display = 'block';
-                document.querySelector('.btn--edit').style.display = 'none';
-            }
+            const todoList = document.getElementById(id);
+            todos.forEach((todo) => {
+                if (todo.id == id)
+                {
+                    if(event.target.className === "btn--edit"){
+                        todoList.querySelector(".todo-list span").style.display = 'none';
+                        todoList.querySelector('.todo-input').style.display = 'block';
+                        todoList.querySelector('.btn--editFin').style.display = 'block';
+                        todoList.querySelector('.btn--edit').style.display = 'none';
+                    }
+                }
+            });
+        })
+        finishedEl.addEventListener('click', function(event) {
+            const id = event.target.id;
+            const List = document.getElementById(id);
+            todos.forEach((todo) => {
+                if (todo.id == id)
+                {
+                    if(event.target.className === "btn--edit"){
+                        List.querySelector(".finished span").style.display = 'none';
+                        List.querySelector('.todo-input').style.display = 'block';
+                        List.querySelector('.btn--editFin').style.display = 'block';
+                        List.querySelector('.btn--edit').style.display = 'none';
+                    }
+                }
+            });
         })
     };
 
     return {
         formEl,
         todoListEl,
+        finishedEl,
         updateTodoList,
-        statusTodoList,
+        //statusTodoList,
         editmode,
     };
 })();
-
 //window.console.log
-
 /* 
     prevent the refresh
     get the value from input
@@ -166,14 +168,12 @@ const ViewModel = ((View, Model) => {
     const addTodo = () => {
         View.formEl.addEventListener("submit", (event) => {
             event.preventDefault();
-            console.log(event.target); //form
             const title = event.target[0].value;  //input.value
             if(title.trim() === "") {
                 alert("please input title!");
                 return;
             }
-            console.log("title", title);
-            const newTodo = { title };
+            const newTodo = { title, status: false };
             Model.addTodo(newTodo)
                 .then((res) => {
                     console.log(res);
@@ -187,55 +187,149 @@ const ViewModel = ((View, Model) => {
         });
     };
 
-    
-    const editTodo = () => { // edit to do list
-       
-        View.todoListEl.addEventListener("click",(event) => {
-            event.preventDefault();
-            const id = event.target.id;
-            console.log(id);
-            console.log("current target ",event.currentTarget.id = id)
-            if (event.currentTarget.id = id){
-                if(event.target.className === "btn--editFin")
-                {
-                        console.log("ednit fin");
-                        const title = document.querySelector('.todo-input').value;
-                        console.log("todoInput", title);
-                        const newTodo = { title };
-                        console.log("newTodo", newTodo);
-
-                        document.querySelector(".todo-list span").style.display = 'block';
-                        document.querySelector('.todo-input').style.display = 'none';
-                        document.querySelector('.btn--editFin').style.display = 'none';
-                        document.querySelector('.btn--edit').style.display = 'block';
-
-                        Model.editTodo(id,newTodo)
-                        .then((res) => {
-                            console.log("res", res);
-                            state.todos = [res, ...state.todos];
-                        })
-                        .catch((err) => {
-                            alert(`Edit task failed: ${err}`);
-                        });
-                }    
-            }      
-        })
-    };
-
-    const statusTodo = () => {
-        View.todoListEl.addEventListener("click",(event)=>{
-            const id = event.target.id;
+    const statusUpdateTodoList = () => {
+        View.todoListEl.addEventListener('click', function(event) {
+            const id = event.target.getAttribute("id");
+            const todoTarget = document.getElementById(id);
             if (event.target.tagName === 'LI') {
-                event.target.classList.toggle('checked');
+                    event.target.classList.toggle('checked');
+                    //console.log(event.target.className == 'checked'); //true
+                    const title = todoTarget.querySelector('span').innerHTML;
+                    console.log("title", event.target.className == 'checked');
+                    const newTodo = {
+                        id: id,
+                        title: title,
+                        status: true
+                        }
+                    Model.editTodo(id,newTodo).then((res) => {
+                        state.todos = state.todos.map((todo) => {
+                            if (todo.id == id)
+                            {
+                                todo = res; 
+                            }
+                            return todo;
+                        })
+                    })
             }
             
-        },false)
+
+        }, false);
+        
+        View.finishedEl.addEventListener('click', function(event) {
+            const id = event.target.getAttribute("id");
+            const todoTarget = document.getElementById(id);
+            if (event.target.tagName === 'LI') {
+                    event.target.classList.toggle('checked');
+                    //console.log(event.target.className == 'checked'); //true
+                    const title = todoTarget.querySelector('span').innerHTML;
+                    
+                        const newTodo = {
+                            id: id,
+                            title: title,
+                            status: false
+                        }
+                    
+                    Model.editTodo(id,newTodo).then((res) => {
+                        state.todos = state.todos.map((todo) => {
+                            if (todo.id == id)
+                            {
+                                todo = res; 
+                            }
+                            return todo;
+                        })
+                    })
+
+            }
+
+        }, false);
+
+        
+    };
+
+    
+    const editTodo = () => { // edit to do list
+        //finishedEl
+        console.log("editTo");
+        View.todoListEl.addEventListener("click",(event) => {
+            event.preventDefault();
+            const id = event.target.getAttribute("id");
+            const todoTarget = document.getElementById(id);
+            if(event.target.className === "btn--editFin")
+            {
+                const title = todoTarget.querySelector('.todo-input').value; //new Title
+                //create new todo object
+                let newTodo = {
+                    "id": id,
+                    "title": title
+                }
+                //change css style
+                todoTarget.querySelector(".todo-list span").style.display = 'block';
+                todoTarget.querySelector('.todo-input').style.display = 'none';
+                todoTarget.querySelector('.btn--editFin').style.display = 'none';
+                todoTarget.querySelector('.btn--edit').style.display = 'block';
+                //The list is not updated...
+                Model.editTodo(id,newTodo).then((res) => {
+                    state.todos = state.todos.map((todo) => {
+                        if (todo.id == id)
+                        {
+                            todo = res; 
+                        }
+                        return todo;
+                    })
+                })
+                .catch((err) => {
+                    alert(`Edit task failed: ${err}`);
+                });
+            }    
+               
+        })
+
+        View.finishedEl.addEventListener("click",(event) => {
+            event.preventDefault();
+            const id = event.target.getAttribute("id");
+            const todoTarget = document.getElementById(id);
+            if(event.target.className === "btn--editFin")
+            {
+                const title = todoTarget.querySelector('.todo-input').value; //new Title
+                //create new todo object
+                let newTodo = {
+                    "id": id,
+                    "title": title
+                }
+                //change css style
+                todoTarget.querySelector(".finished span").style.display = 'block';
+                todoTarget.querySelector('.todo-input').style.display = 'none';
+                todoTarget.querySelector('.btn--editFin').style.display = 'none';
+                todoTarget.querySelector('.btn--edit').style.display = 'block';
+                //The list is not updated...
+                Model.editTodo(id,newTodo).then((res) => {
+                    state.todos = state.todos.map((todo) => {
+                        if (todo.id == id)
+                        {
+                            todo = res; 
+                        }
+                        return todo;
+                    })
+                })
+                .catch((err) => {
+                    alert(`Edit task failed: ${err}`);
+                });
+            }    
+               
+        })
     };
 
     const removeTodo = () => {
         //event bubbling: event listener from parent element can receive event emitted from its child
         View.todoListEl.addEventListener("click",(event)=>{
-            //console.log(event.target/* emit the event */, event.currentTarget/* receive the event */);
+            const id = event.target.id;
+            if(event.target.className === "btn--delete"){
+                Model.removeTodo(id).then(res=>{
+                    state.todos = state.todos.filter(todo => +todo.id !== +id)
+                }).catch(err=>alert(`delete todo failed: ${err}`))
+            }
+        })
+        View.finishedEl.addEventListener("click",(event)=>{
             const id = event.target.id;
             if(event.target.className === "btn--delete"){
                 Model.removeTodo(id).then(res=>{
@@ -250,10 +344,10 @@ const ViewModel = ((View, Model) => {
         getTodos();
         removeTodo();
         editTodo();
-        statusTodo();
+        statusUpdateTodoList();
         state.subscribe(() => {
             View.updateTodoList(state.todos);
-            View.statusTodoList(state.todos);
+            //View.statusTodoList(state.todos);
             View.editmode(state.todos);
         });
     };
